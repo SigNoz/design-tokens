e096999a81ff4e1d52d69242cbf2dc19dacd301e
ahmadshaheer <ashaheerki@gmail.com>
2024-10-09 21:19:33 +0430

    feat: generate design tokens from json and overall improvements

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,4 @@
-import { Color } from "./Color";
-import { Spacing } from "./Spacing";
-import { Typography } from "./Typography";
-
-export { Color, Spacing, Typography };
-
-export function generateCSSVariables(): string {
-  const generateVars = (obj: Record<string, string>) =>
-    Object.entries(obj).map(([key, value]) => {
-      const cssVarName = `--${key.toLowerCase().replace(/_/g, "-")}`;
-      return `${cssVarName}: ${value};`;
-    });
-
-  const allVars = [
-    ...generateVars(Color),
-    ...generateVars(Typography),
-    ...generateVars(Spacing),
-  ];
-
-  return `:root {\n  ${allVars.join("\n  ")}\n}`;
-}
+export * from './Colors/Color';
+export * from './Colors/ColorTailwind';
+export * from './Spacing/Spacing';
+export * from './Typography/Typography';
