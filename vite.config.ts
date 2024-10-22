0c7deff185b4d9991669bf47f5e61e2c3d0722b3
ahmadshaheer <ashaheerki@gmail.com>
2024-10-22 15:42:56 +0430

    fix: fix the issue for consumers (unable to find type declarations)

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,23 @@
-import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
-import { resolve } from "path";
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import { resolve } from 'path';
 
 export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      name: "designTokens",
-      fileName: "design-tokens",
-    },
-    rollupOptions: {
-      external: [],
-    },
-  },
-  plugins: [dts()],
+	build: {
+		lib: {
+			entry: resolve(__dirname, 'src/index.ts'),
+			name: 'designTokens',
+			fileName: 'design-tokens',
+		},
+		rollupOptions: {
+			external: [],
+		},
+	},
+	plugins: [
+		dts({
+			include: ['src/**/*'],
+			outDir: 'dist',
+			rollupTypes: true,
+		}),
+	],
 });
