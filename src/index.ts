85f649ab147a6199135e30eba0723dfda6744502
ahmadshaheer <ashaheerki@gmail.com>
2024-10-23 13:57:02 +0430

    chore: export design tokens json files

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@ export * from './Colors/Color';
 export * from './Colors/ColorTailwind';
 export * from './Spacing/Spacing';
 export * from './Typography/Typography';
+export { default as colorTokens } from './tokens/color.json';
+export { default as spacingTokens } from './tokens/spacing.json';
+export { default as typographyTokens } from './tokens/typography.json';
