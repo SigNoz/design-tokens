4962a0d6601bce7610fa2b1b98cd62393836b9f0
ahmadshaheer <ashaheerki@gmail.com>
2024-10-23 20:09:45 +0430

    fix: make the package backwards compatible by exporting style.css

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import { resolve } from 'path';
+import postcss from 'postcss';
+import fs from 'fs';
+import cssnano from 'cssnano';
 
 export default defineConfig({
 	build: {
@@ -19,5 +22,34 @@ export default defineConfig({
 			outDir: 'dist',
 			rollupTypes: true,
 		}),
+		{
+			name: 'copy-css',
+			writeBundle() {
+				// Process the CSS file with PostCSS before copying
+
+				const cssFilePath = resolve(__dirname, 'src/style.css');
+				const outputFilePath = resolve(__dirname, 'dist/style.css');
+
+				// Read the CSS file
+				fs.readFile(cssFilePath, (err, css) => {
+					if (err) throw err;
+
+					// Process the CSS with PostCSS
+					postcss([cssnano({ preset: 'default' })])
+						.process(css, { from: cssFilePath, to: outputFilePath })
+						.then((result) => {
+							// Write the minified CSS to the dist folder
+							fs.writeFile(outputFilePath, result.css, () => true);
+							if (result.map) {
+								fs.writeFile(
+									outputFilePath + '.map',
+									result.map.toString(),
+									() => true,
+								);
+							}
+						});
+				});
+			},
+		},
 	],
 });
